e8a25dee6ad226a0acc23ba06d15c791db47674d
Raj kumar gupta <grajkumar280@gmail.com>
2022-03-10 10:27:57 +0530

    Add files via upload

---
+++ WEB Scrapping/headers.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  9 09:38:10 2022
+
+@author: raj
+"""
+
+headers ={'authority' : 'www.amazon.de',
+           'method': 'GET',
+           'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+           'accept-encoding': 'gzip, deflate, br',
+           'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7',
+            'referer': productUrl,
+            'rtt': '50' ,
+            'sec-ch-ua-platform': 'Linux'
+            'sec-fetch-dest': 'image'
+             'sec-fetch-mode': 'no-cors'
+             'sec-fetch-site': 'same-origin'
+             'user-agent': 
+                 
+headers = {
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': arg_user_agent,
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': productUrl,
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
